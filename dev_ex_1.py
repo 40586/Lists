24138f9e31777bff99cb115c01ca1f40b6e187e7
40586 <40586@longroad.ac.uk>
2014-12-12 14:31:10 +0000

    12/12/2014

---
+++ dev_ex_1.py
@@ -0,0 +1,4 @@
+#Kieran burnett
+#development exercise 1
+cake = "It's a lie"
+leon = "THE DEVIL HIMSELF"
---
+++ starter2.py
@@ -0,0 +1,8 @@
+values = [24,13,57,45]
+result = 0
+
+for index in values:
+    if result < index:
+        result = index
+print(result)
+
